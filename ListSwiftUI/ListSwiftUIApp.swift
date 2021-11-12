062d4576b08d75730b6b710a6581e172fc4eeb1a
Dmitri Gordeev <dimapushkin@gmail.com>
2021-11-12 17:34:54 +0300

    Initial Commit

---
+++ ListSwiftUI/ListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSwiftUIApp.swift
+//  ListSwiftUI
+//
+//  Created by Повелитель on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
