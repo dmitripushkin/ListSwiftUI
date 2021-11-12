d35ecc138aad781e407bc3bdd672bf9d7125c253
Dmitri Gordeev <dimapushkin@gmail.com>
2021-11-12 18:16:32 +0300

    Add Tab Bar

--- ListSwiftUI/ContentView.swift
+++ ListSwiftUI/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("First View")
+                .tabItem {
+                    Image(systemName: "figure.roll")
+                    Text("Contacts list")
+                }
+            Text("Second View")
+                .tabItem {
+                    Image(systemName: "person.text.rectangle.fill")
+                    Text("Sections")
+                }
+        }
     }
 }
 
